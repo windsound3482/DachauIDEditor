2fe6b663f164bc8561ae8b95df0afc0f2d2c64fb
Jilin Liao <jilin.liao@siemens.com>
2023-10-07 14:13:32 +0200

    add in frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+const port = 3000
+app.get('/', function (req, res) {
+  res.json(
+    { [
+      K071270:{
+        Nachname:"KAUFMANN",
+        Vorname:"MICHAEL",
+        Geburtsort:"Csikszerada",
+        GebTag
+      }
+      			3	11	1897	Auschwitz	18/06/44	AL 3	AL 1	23/01/45		m
+  ]})
+    
+})
+
+Geburtsort
+
+Nachname	Vorname			GebMon	GebJahr	Zugang von	Zugang Datum	Zugang in	Überführt nach	Überführt Da	Gestorben	Geschlecht
+
+
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
